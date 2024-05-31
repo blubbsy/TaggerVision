c2c9002104e560812f7cc04478c507e1e7ca5424
Adrian <Adrian@adrilaida.de>
2024-05-31 15:55:12 +0200

    chore: Update image processing script to use new source and target directories

--- imagetagging.py
+++ imagetagging.py
@@ -4,11 +4,12 @@ import os
 from io import BytesIO
 from pathlib import Path
 from PIL import Image
-from iptcinfo3 import IPTCInfo
+#from iptcinfo3 import IPTCInfo
 from langchain_community.llms import Ollama
 import exiftool
 import json
 import datetime
+from tqdm import tqdm
 
 # Constants for directories and prompts
 OLLAMA_BASE_URL = "http://127.0.0.1:11434"
@@ -17,12 +18,11 @@ TARGET_DIR = "C:/Users/Sibby/Downloads/Image_KeywordTagging/examples"
 SOURCE_DIR = "Z:/Photos/"
 TARGET_DIR = "Z:/Photos/"
 
-PROMPT_KEYWORDS = "Please provide precise keywords separated by commas."
-PROMPT_DESCRIPTION = "Please provide a description."
-PROMPT_TITLE = "Please give the photo a title."
+PROMPT_KEYWORDS = "Please provide a minimum of 10 precise keywords separated by commas."
+PROMPT_DESCRIPTION = "Please provide a description of what is on the photo. Avoid repetitive sentences and words."
+PROMPT_TITLE = "Please give the photo an artistic title. Avoid naming locations or people."
 
 FILE_EXTENSIONS = ['*.jpeg', '*.jpg', '*.png']
-
 
 # Custom EXIF tag for marking processed images
 CUSTOM_EXIF_TAG = "-Custom:ProcessedByTaggerVision"
@@ -40,12 +40,14 @@ def convert_to_base64(pil_image):
 # Function to process image with LLama model
 def process_image(image_path, prompt):
     # Connect to LLama 1.6
-    llava_model = Ollama(model="llava:v1.6", base_url=OLLAMA_BASE_URL, temperature=0)
+    #llava_model = Ollama(model="llava:v1.6", base_url=OLLAMA_BASE_URL, temperature=0)
+    #llava_model = Ollama(model="llava-llama3", base_url=OLLAMA_BASE_URL, temperature=0)
+    llava_model = Ollama(model="llava-phi3", base_url=OLLAMA_BASE_URL, temperature=0)
     
     try:
         # Read the image
         print(f"Processing image '{image_path}'...")
-        info = IPTCInfo(image_path, force=True, inp_charset='utf8')
+        #info = IPTCInfo(image_path, force=True, inp_charset='utf8')
         pil_image = Image.open(image_path)
 
         # Resize the image to a width of 672 pixels
@@ -118,14 +120,19 @@ def is_image_processed(image_path):
         return True
 
 if __name__ == "__main__":
-    # Iterate through files in the specified directory  
+    # Collect all files to be processed
+    all_files = []
+    print("Collecting file list...")
     for ext in FILE_EXTENSIONS:
-        for filepath in glob.glob(SOURCE_DIR + '/**/' + ext, recursive=True):
-            # Skip processing if the image is already tagged
-            if not FORCE_REPROCESS:
-                if is_image_processed(filepath):
-                    continue
-            
-            process_image(filepath, PROMPT_TITLE)
-            process_image(filepath, PROMPT_KEYWORDS)
-            process_image(filepath, PROMPT_DESCRIPTION)
+        all_files.extend(glob.glob(SOURCE_DIR + '/**/' + ext, recursive=True))
+        
+    # Iterate through files with a progress bar
+    for filepath in tqdm(all_files, desc="Processing images"):
+        # Skip processing if the image is already tagged
+        if not FORCE_REPROCESS:
+            if is_image_processed(filepath):
+                continue
+
+        process_image(filepath, PROMPT_TITLE)
+        process_image(filepath, PROMPT_KEYWORDS)
+        process_image(filepath, PROMPT_DESCRIPTION)
